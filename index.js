6855a679f9c36f14acf06697da4652373cdfa3d7
Vikas N <vikassharadha2006@gmail.com>
2025-06-01 15:29:40 +0530

    Initial setup and authorization and tokens setup

--- index.js
+++ index.js
@@ -2,9 +2,28 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const port = 8000;
+const userRoute = require('./routes/user');
+const connectMongoDb = require('./connection');
+require('dotenv').config();
+
+connectMongoDb("mongodb://127.0.0.1:27017/blogDb")
+    .then(() => {
+        console.log("Connected to MongoDB successfully");
+    })
+    .catch((err) => {
+        console.error("Error connecting to MongoDB:", err);
+    })
 
 app.set('view engine', 'ejs');
 app.set('views', path.resolve('./views'));
+
+
+app.get('/', (req, res) => {
+    return res.render("homepage");
+})
+
+app.use(express.urlencoded({ extended: true }));
+app.use('/user', userRoute);
 
 app.listen(port, () => {
     console.log(`Server started at ${port}`);
